bfc662a631b21ab4e256c1b6a70c440b7618b82f
Jóannes í Sandagerði <eplamos.mos@gmail.com>
2017-01-15 15:40:39 +0100

    Added chmod to docker-compose.

--- docker.sh
+++ docker.sh
@@ -7,4 +7,7 @@ sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 sudo apt-get install -y docker-engine
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+# docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
