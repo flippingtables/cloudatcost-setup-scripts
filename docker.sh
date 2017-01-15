d8c0e493a190000302a4bd175c7a55e5367490c5
Jóannes í Sandagerði <eplamos.mos@gmail.com>
2017-01-15 17:38:13 +0100

    Add unzip to docker.sh.

--- docker.sh
+++ docker.sh
@@ -11,3 +11,6 @@ sudo usermod -aG docker $USER
 # docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# others
+sudo apt-get install -y unzip
